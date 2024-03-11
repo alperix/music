d2182c4f61761cf08579d337b3d377a3abb70339
alperix <alper.ozagac@gmail.com>
2024-03-11 12:05:00 +0100

    tailwind optiimization disabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 import plugin from 'tailwindcss/plugin'
 
-// css bundle optimization
-const colors = ["black", "white", "red", "green", "blue"];
-const prefixes = ["bg", "border", "text", "placeholder"];
+// // css bundle optimization
+// const colors = ["black", "white", "red", "green", "blue"];
+// const prefixes = ["bg", "border", "text", "placeholder"];
 
-// This includes bg of all necessary colors and shade
-const regex = () => {
-    const color = colors.join("|");
-    const pres = prefixes.map((p) => `${p}-(${color})`).join("|");
-    return new RegExp(`(${pres})-+`);
-};
+// // This includes bg of all necessary colors and shade
+// const regex = () => {
+//     const color = colors.join("|");
+//     const pres = prefixes.map((p) => `${p}-(${color})`).join("|");
+//     return new RegExp(`(${pres})-+`);
+// };
 
 module.exports = {
     content: ["./dist/**/*.html", "./src/**/*.{js,jsx,ts,tsx}", "./*.html"],
-    safelist: [{ pattern: regex() }],
+    //safelist: [{ pattern: regex() }],
     plugins: [
         plugin(function({ addBase, theme }) {
           addBase({
