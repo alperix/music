d1003799f2ccb63b96787bfd77f39015719cf144
alperix <alper.ozagac@gmail.com>
2024-03-26 18:34:05 +0100

    samples

---
+++ src/core/services/Resources.ts
@@ -0,0 +1,36 @@
+import texts from "@/core/resources/Texts.json";
+
+type NestedText = {
+    [key: string]: NestedText | string;
+};
+
+export const getResource = (obj: NestedText, path: string): string => {
+    const props = path.split(".");
+
+    if (props.length === 1) {
+        return obj[path] as string;
+    }
+
+    const [cur, ...rest] = props;
+    return getResource(obj[cur] as NestedText, rest.join("."));
+};
+
+export const setTemplate = (text: string, vars: Record<string, string>) => {
+    let result = text;
+
+    Object.keys(vars).forEach(
+        (key) => (result = result.replace(`{${key}}`, vars[key]))
+    );
+
+    return result;
+};
+
+export const Text = (
+    path: string,
+    vars: Record<string, string> | null = null
+) => {
+    let text = getResource(texts, path) || `!Resource[${path}]`;
+    if (vars) text = setTemplate(text, vars);
+    return text;
+};
+
